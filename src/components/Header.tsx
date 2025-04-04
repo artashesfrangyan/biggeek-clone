f2d846d7b96639cb544379598bd6acb0cf0b20e0
afrangyan <afrangyan@kameleoon.com>
2025-04-05 00:52:15 +0300

    added a header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => (
+    <header className="header">
+      <div className="logo">BigGeek Clone</div>
+      <nav>
+        <ul>
+          <li><a href="/">Каталог</a></li>
+          <li><a href="/">Доставка</a></li>
+        </ul>
+      </nav>
+    </header>
+);
